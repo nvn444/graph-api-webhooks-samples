1631ee150600acc2c4c1db581f32243260da018d
nvn444 <117005733+nvn444@users.noreply.github.com>
2022-11-08 17:40:28 +0530

    test3

--- heroku/index.js
+++ heroku/index.js
@@ -16,13 +16,13 @@ app.listen(app.get('port'));
 
 app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
 app.use(bodyParser.json());
-
+console.log('reached here');
 var token = process.env.TOKEN || 'token';
 var received_updates = [];
 
 app.get('/', function(req, res) {
   console.log(req);
-  console.log('this is a test only')
+  console.log('this is a test only');
   res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
 });
 
@@ -39,7 +39,7 @@ app.get(['/facebook', '/instagram'], function(req, res) {
 
 app.post('/facebook', function(req, res) {
   console.log('Facebook request body:', req.body);
-console.log('testing 1 2 3 !!!')
+console.log('testing 1 2 3 !!!');
   if (!req.isXHubValid()) {
     console.log('Warning - request header X-Hub-Signature not present or invalid');
     res.sendStatus(401);
