50f21e14963311b5d683e8d4c67d31b6ac25be91
nvn444 <117005733+nvn444@users.noreply.github.com>
2022-11-08 17:34:29 +0530

    testing2

--- heroku/index.js
+++ heroku/index.js
@@ -22,7 +22,7 @@ var received_updates = [];
 
 app.get('/', function(req, res) {
   console.log(req);
-  console.log("this is a test only")
+  console.log('this is a test only')
   res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
 });
 
@@ -39,7 +39,7 @@ app.get(['/facebook', '/instagram'], function(req, res) {
 
 app.post('/facebook', function(req, res) {
   console.log('Facebook request body:', req.body);
-console.log("testing 1 2 3 !!!")
+console.log('testing 1 2 3 !!!')
   if (!req.isXHubValid()) {
     console.log('Warning - request header X-Hub-Signature not present or invalid');
     res.sendStatus(401);
