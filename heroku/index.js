4998531c1ff7a70be92275940becbcc2f54d447d
nvn444 <117005733+nvn444@users.noreply.github.com>
2022-11-08 17:31:52 +0530

    testing123

--- heroku/index.js
+++ heroku/index.js
@@ -39,7 +39,7 @@ app.get(['/facebook', '/instagram'], function(req, res) {
 
 app.post('/facebook', function(req, res) {
   console.log('Facebook request body:', req.body);
-
+console.log("testing 1 2 3 !!!")
   if (!req.isXHubValid()) {
     console.log('Warning - request header X-Hub-Signature not present or invalid');
     res.sendStatus(401);
