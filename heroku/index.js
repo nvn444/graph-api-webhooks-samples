8306bb0015e7670e5ab8d64118fc9194c12d7c8a
nvn444 <117005733+nvn444@users.noreply.github.com>
2022-11-11 09:23:19 +0530

    t9

--- heroku/index.js
+++ heroku/index.js
@@ -112,5 +112,24 @@ const options = {
   }
 };
 
+const req = https.request(options, (res) => {
+  let data = '';
+
+  console.log('Status Code:', res.statusCode);
+
+  res.on('data', (chunk) => {
+      data += chunk;
+  });
+
+  res.on('end', () => {
+      console.log('Body: ', JSON.parse(data));
+  });
+
+}).on("error", (err) => {
+  console.log("Error: ", err.message);
+});
+
+req.write(data);
+req.end();
 
 app.listen();
