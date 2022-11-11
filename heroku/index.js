779001d132c5e45b37d1409473060b5a095e6251
nvn444 <117005733+nvn444@users.noreply.github.com>
2022-11-11 09:33:50 +0530

    t9

--- heroku/index.js
+++ heroku/index.js
@@ -116,16 +116,8 @@ const url = 'https://jsonplaceholder.typicode.com/todos/1';
   
 const request = https.request(url,response);
   
-    response.on('end', () => {
-        const body = JSON.parse(data);
-        console.log(body);
-    });
-})
+
   
-request.on('error', (error) => {
-    console.log('An error', error);
-});
-  
-request.end(); 
+//request.end(); 
 
 app.listen();
